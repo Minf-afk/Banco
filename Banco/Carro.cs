a54aea678667a866c3674a4aa82dcb8b64eb42a4
Mateus de Santana Pereira <msantana.inf@gmail.com>
2026-02-06 16:50:00 -0300

    Implantação do carro

--- Banco/Carro.cs
+++ Banco/Carro.cs
@@ -1,10 +1,35 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Xml;
 
 namespace Banco
 {
-    internal class Carro
+
+    public class Carro
     {
+        public string Modelo;
+        public int Velocidade;
+        public string Tipo_combustivel;
+        public string Segurança;
+
+        public void InfoCarro()
+        {
+            Console.WriteLine($"O carro {Modelo} tem velocidade máxima de {Velocidade} e tem o tipo de combustível {Tipo_combustivel} e possui o acessório de Segurança: {Segurança}\n");
+        }
+        public void Acelerar()
+        {
+            Console.WriteLine($"O {Modelo} tem velocidade máxima de {Velocidade}\n");
+        }
+        public void Frear()
+        {
+            Console.WriteLine($"O {Modelo} está freando\n");
+        }
+
+        public void Buzinar()
+        {
+            Console.WriteLine($"BIIIIIIIIIIII!!!\n");
+        }
     }
+
 }
--- Banco/Program.cs
+++ Banco/Program.cs
@@ -1,32 +1,43 @@
 ﻿using Banco;
 
-ContaBanco Maria =  new ContaBanco();
+//ContaBanco Maria =  new ContaBanco();
 
-Maria.numero_indicador = 1;
-Maria.titular = "Maria de Jesus";
-Maria.saldo = 1542.23;
-Maria.senha = 123456;
+//Maria.numero_indicador = 1;
+//Maria.titular = "Maria de Jesus";
+//Maria.saldo = 1542.23;
+//Maria.senha = 123456;
 
-Console.WriteLine($"Digite seu nome completo:  \n");
-string digitacaoTitular = Console.ReadLine();
-if (digitacaoTitular == Maria.titular)
-{
-    Console.WriteLine("Usuário autenticado devidamente \n");
-}
-else
-{
-    Console.WriteLine("Usuário não reconhecido\n");
-}
-Console.WriteLine("Digite a senha do usuário: \n");
-int senhaUsuario = int.Parse(Console.ReadLine());
-if (senhaUsuario == Maria.senha)
-{
-    Console.WriteLine($"Usuário logado, saldo da conta: {Maria.saldo}");
-}
-else
-{
-    Console.WriteLine("Senha errado, favor tentar novamente;");
-}
+//Console.WriteLine($"Digite seu nome completo:  \n");
+//string digitacaoTitular = Console.ReadLine();
+//if (digitacaoTitular == Maria.titular)
+//{
+//    Console.WriteLine("Usuário autenticado devidamente \n");
+//}
+//else
+//{
+//    Console.WriteLine("Usuário não reconhecido\n");
+//}
+//Console.WriteLine("Digite a senha do usuário: \n");
+//int senhaUsuario = int.Parse(Console.ReadLine());
+//if (senhaUsuario == Maria.senha)
+//{
+//    Console.WriteLine($"Usuário logado, saldo da conta: {Maria.saldo}");
+//}
+//else
+//{
+//    Console.WriteLine("Senha errado, favor tentar novamente;");
+//}
 
 
-Maria.ExibirInformacoes();
+//Maria.ExibirInformacoes();
+
+Carro Fox = new Carro();
+Fox.Segurança = "Air Bag";
+Fox.Velocidade = 200;
+Fox.Modelo = "Fox 2010 1.6";
+Fox.Tipo_combustivel = "Flex";
+
+Fox.InfoCarro();
+Fox.Buzinar();
+Fox.Acelerar();
+Fox.Frear();
