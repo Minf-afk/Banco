d62e0f3f514dd8d9272c2e783319d797d327634f
Mateus de Santana Pereira <msantana.inf@gmail.com>
2026-02-06 15:29:34 -0300

    Inicio do projeto de objetos;.

---
+++ Banco/Banco.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Banco
+{
+    public class ContaBanco
+    {
+        public int numero_indicador;
+        public string titular;
+        public double saldo;
+        public int senha;
+    }
+}
--- Banco/Program.cs
+++ Banco/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Banco;
+
+ContaBanco Maria =  new ContaBanco();
+
+Maria.numero_indicador = 1;
+Maria.titular = "Maria de Jesus";
+Maria.saldo = 1542.23;
+Maria.senha = 123456;
+
+Console.WriteLine($"Digite por favor seu nome completo:  \n");
+string digitacaoTitular = Console.ReadLine();
+if (digitacaoTitular != Maria.titular)
+{
+    Console.WriteLine("Usuário autenticado devidamente ");
+}
+else
+{
+    Console.WriteLine("Usuário não reconhecido");
+}
