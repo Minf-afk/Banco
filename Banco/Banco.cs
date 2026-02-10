4214d315c9ba25f5fafff21f51093607be746c83
Mateus de Santana Pereira <msantana.inf@gmail.com>
2026-02-10 10:14:43 -0300

    Ajuste do objeto

--- Banco/Banco.cs
+++ Banco/Banco.cs
@@ -1,24 +1,23 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
 
 namespace Banco
 {
     public class Titular
     {
-            public string NomeCompleto {  get; set; }
-            public int Rg {  get; set; }
-            public long Cpf { get; set; }
+        public string NomeCompleto { get; set; } = "";
+        public int Rg { get; set; }
+        public long Cpf { get; set; }
     }
+
     public class Conta
     {
-        public Titular;
+        public Titular Titular { get; set; } = new Titular();
         public int Agencia { get; set; }
         public int NumeroConta { get; set; }
         public decimal Saldo { get; set; }
         public decimal Limite { get; set; }
 
-        public string MostrarInfo => $"O Sr(a) {Titular} está logado na conta {Agencia}/{NumeroConta} possuindo o valor de : {Saldo} e Limite : {Limite} ";
+        public string MostrarInfo =>
+            $"O Sr(a) {Titular.NomeCompleto} está logado na conta {Agencia}/{NumeroConta} possuindo o saldo de: {Saldo:C} e limite: {Limite:C}.";
     }
-}   
-    
+}
