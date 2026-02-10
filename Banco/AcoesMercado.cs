10f20c71b3c089198af5961884ac9eccec34db7a
Mateus de Santana Pereira <msantana.inf@gmail.com>
2026-02-10 10:31:31 -0300

    Adição da props e adicionando devidamente as functions

---
+++ Banco/AcoesMercado.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Banco
+{
+    public class AcoesMercado
+    {
+        Mercado adicaoProduto = new Mercado();
+        void AdicionarPorduto()
+        {
+            Console.WriteLine("Digite o nome do produto que deseja adicionar ao sistema: ");
+            adicaoProduto.Produto = Console.ReadLine();
+            Console.Clear();
+
+            Console.WriteLine("");
+        }
+
+        void ListarProdutos()
+        {
+            List <string> ListandoProd = new List<string> ();
+            
+        }
+
+        void ExcluirProdutos()
+        {
+
+        }
+    }
+}
---
+++ Banco/Mercado.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+
+namespace Banco
+{
+    public class Mercado
+    {
+        public string Produto { get; set; }
+        public string Marca { get; set; }
+        public int Quantidade { get; set; }
+        public int DisponiveisEmEstoque { get; set; }
+    }
+
+}
